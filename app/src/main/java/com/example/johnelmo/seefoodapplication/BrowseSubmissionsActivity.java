c559da86069113ff559a3d6f89fe4bcb836528ad
John <elmo.4@wright.edu>
2018-11-04 00:02:12 -0400

    Added activites for camera and browsing past submissions

---
+++ app/src/main/java/com/example/johnelmo/seefoodapplication/BrowseSubmissionsActivity.java
@@ -0,0 +1,13 @@
+package com.example.johnelmo.seefoodapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BrowseSubmissionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse_submissions);
+    }
+}
