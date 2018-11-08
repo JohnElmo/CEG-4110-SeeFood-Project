ee7433a353dfcbcc86d2a8e426811f63ac9ce80e
John <elmo.4@wright.edu>
2018-11-08 17:09:57 -0500

    Added camera functionality to application

--- app/src/main/java/com/example/johnelmo/seefoodapplication/CameraActivity.java
+++ app/src/main/java/com/example/johnelmo/seefoodapplication/CameraActivity.java
@@ -1,20 +1,33 @@
 package com.example.johnelmo.seefoodapplication;
 
 import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.github.clans.fab.FloatingActionButton;
 
 public class CameraActivity extends AppCompatActivity {
 
     FloatingActionButton selectHomeFab, selectImageFab, selectBrowseFab;
+    Button capture;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        capture = findViewById(R.id.captureButton);
+        capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+
         selectHomeFab = (FloatingActionButton) findViewById(R.id.fab_Camera_HomeSelect);
         selectImageFab = (FloatingActionButton) findViewById(R.id.fab_Camera_ImageSelect);
         selectBrowseFab = (FloatingActionButton) findViewById(R.id.fab_Camera_BrowseSubmissions);
@@ -41,6 +54,13 @@ public class CameraActivity extends AppCompatActivity {
         });
     }
 
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
     public void changeToMainActivity(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
@@ -55,4 +75,5 @@ public class CameraActivity extends AppCompatActivity {
         Intent intent = new Intent(this, BrowseSubmissionsActivity.class);
         startActivity(intent);
     }
+
 }
