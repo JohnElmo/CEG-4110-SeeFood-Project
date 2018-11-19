c7cac31f7059186ca8c42d26728e772f09f5259a
Ethan <dennewitz.3@wright.edu>
2018-11-19 16:18:02 -0500

    basic functionality added to select image activity button accesses the device's gallery and previews the selected image, submit button added

--- app/src/main/java/com/example/johnelmo/seefoodapplication/ImageSelectActivity.java
+++ app/src/main/java/com/example/johnelmo/seefoodapplication/ImageSelectActivity.java
@@ -4,7 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.view.View.OnClickListener;
+
 
 import com.github.clans.fab.FloatingActionButton;
 
@@ -12,6 +19,11 @@ public class ImageSelectActivity extends AppCompatActivity {
 
     FloatingActionButton selectHomeFab, selectCameraFab, selectBrowseFab;
     ImageButton selectHelp;
+
+    private static final int SELECT_PICTURE = 1;
+
+    private String selectedImagePath;
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,8 +64,38 @@ public class ImageSelectActivity extends AppCompatActivity {
 
         });
 
+        img = (ImageView)findViewById(R.id.GalleryPreview);
+
+        ((Button) findViewById(R.id.Gallerybtn))
+                .setOnClickListener(new OnClickListener() {
+                    public void onClick(View arg0) {
+                        Intent intent = new Intent();
+                        intent.setType("image/*");
+                        intent.setAction(Intent.ACTION_GET_CONTENT);
+                        startActivityForResult(Intent.createChooser(intent,"Select Picture"), SELECT_PICTURE);
+                    }
+                });
+
+
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                Uri selectedImageUri = data.getData();
+                selectedImagePath = getPath(selectedImageUri);
+                System.out.println("Image Path : " + selectedImagePath);
+                img.setImageURI(selectedImageUri);
+            }
+        }
     }
 
+    public String getPath(Uri uri) {
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
     public void changeToMainActivity(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
